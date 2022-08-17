403b0e7fdb2f2dfb7b0b6fc0cc774c71b7acc5a9
Ayush Walia <ayush0147.be20@chitkara.edu.in>
2022-08-17 09:31:39 +0530

    Final Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var http=new XMLHttpRequest();
+http.onreadystatechange=function() {
+    var output=document.getElementById("albm-div");
+    if(http.status==200 && http.readyState==4){
+        var obj=JSON.parse(this.response);
+        for(var i=0;i<300;i++)
+        {
+            output.innerHTML+=`
+            <div class="card">
+            <img src="${obj[i].url}" class="img">
+            <div class="inner-card">
+                <span>${obj[i].title}</span>
+            </div>
+        </div>`;
+
+        }
+
+    }
+}
+http.open("GET","https://jsonplaceholder.typicode.com/photos?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09",true );
+http.send();
